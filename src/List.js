d9b086131662a9ffd3be3ecdf35ab24725181994
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-11-24 15:41:22 -0600

    new

--- src/List.js
+++ src/List.js
@@ -1,9 +1,35 @@
+// import React, {Component} from 'react';
 import React from 'react';
+import App from './App';
 
-const List = props => (
+// export default class List extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         isComplete: false
+//       };
+//     }
+//       //delete button when the todo is complete
+//   isComplete = (event) => {
+//     this.setState({isComplete: true})
+//     console.log(this.isComplete)
+//   }
+//   render(){
+//       return (
+//           {isComplete ? }
+//       )
+//   }
+
+// }
+
+
+
+
+const List = (props) => (
     <ul>
         {props.items.map((item, index) => 
-            <li key={index}>{item} <button>Edit</button></li>)}
+            <li key={item.id}>{item.text}<button onClick={() => {props.completed(item, index)}}>Complete</button></li>
+        )}
     </ul>
 );
 
